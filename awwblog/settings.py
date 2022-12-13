7e27d6d160c4b32eb9fbcd8fb62749abc852b949
Horl-Socheath <horl.socheath@gmail.com>
2022-12-13 22:00:08 +0700

    post sitemap

--- awwblog/settings.py
+++ awwblog/settings.py
@@ -31,6 +31,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
+
 
 # Application definition
 
@@ -41,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'blog',
     'ckeditor',
     'ckeditor_uploader',
